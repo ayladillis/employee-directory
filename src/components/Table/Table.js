46418cfbf922cdf7ccb6e4a048faa13ec3f37447
Ayla Dillis <aayladillis@gmail.com>
2020-08-12 18:30:40 -0700

    sudocoding

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,23 +3,24 @@ import "./Table.css"
 
 function Table (props) {
     return (
-        <table className="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Avatar</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Number</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"><img src={props.image}/></th>
-      <td> { props.name } </td>
-      <td> { props.email } </td>
-      <td> { props.number } </td>
-    </tr>
-  </tbody>
+<table className="table table-striped">
+    {/* add filter and sort functions here */}
+    <thead>
+        <tr>
+        <th scope="col">Avatar</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Number</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row"><img src={props.image}/></th>
+        <td> { props.name } </td>
+        <td> { props.email } </td>
+        <td> { props.number } </td>
+        </tr>
+    </tbody>
 </table>
     );
 }
